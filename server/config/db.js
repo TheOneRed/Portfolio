679665c0ddabd363338849855a000c555fe26876
Jeff Li <jeff.li.cf@gmail.com>
2017-12-15 21:15:50 -0500

    database check

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/videogames"
+  "URI" : "mongodb://Jeff:123456@ds139242.mlab.com:39242/portfolio"
   //Remote MongoDB deployment -> let URI = "mongodb://thomas:123456@ds054999.mlab.com:54999/videogames";
 };
